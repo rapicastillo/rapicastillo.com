6e11c7217ddf63b1403f471cddc10b61c6f5bbb7
Rapi Castillo <rapi.castillo@gmail.com>
2019-01-04 11:40:41 -0500

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  siteMetadata: {
+    title: `Rapi Castillo | Tech Worker, Community Organizer, Amateur Baker`,
+    description: `Meet Rapi`,
+    author: `@pikittakbo`,
+  },
+  plugins: [
+    `react-media`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/images`,
+        name: 'images',
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/images/splash-mobile.jpg`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
