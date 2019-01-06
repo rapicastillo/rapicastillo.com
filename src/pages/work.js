cb0791c5a7637d4a0440ee3038948f8775a14992
Rapi Castillo <rapi.castillo@gmail.com>
2019-01-06 03:39:10 -0500

    Work

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Page from '../components/page'
 import SEO from '../components/seo'
@@ -18,7 +17,6 @@ const WorkPage = ({ data: { page: {
           <h1>{title}</h1>
           </div>
           <div className='content-area' dangerouslySetInnerHTML={{__html: html}}></div>
-          <Link to="/">Go back to the homepage</Link>
         </div>
       </Page>
     )
@@ -34,14 +32,10 @@ export const pageQuery = graphql`
         frontmatter {
             title
             image
-        }
-      }
-
-      projects: markdownRemark(frontmatter: {uniq: { eq: "projects" }}) {
-        html
-        frontmatter {
-            title
-            image
+            date
+            endDate
+            annotation
+            skills
         }
       }
     }
