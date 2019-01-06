4b870a436a0965337d8df5eb75c8a9ab943adc34
Rapi Castillo <rapi.castillo@gmail.com>
2019-01-06 11:43:24 -0500

    Work page

--- src/components/page.js
+++ src/components/page.js
@@ -27,7 +27,7 @@ const Page = ({ children, showHeader = true, fullHeader = false, bgImage, classN
             © {new Date().getFullYear()}, Rapi Castillo &bull; Built with{` `}<a href="https://www.gatsbyjs.org">Gatsby</a>
           </div>
           <div className='icon-credit'>
-            <div>Croissant Icon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Homepage Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
           </div>
         </footer>
       </div>
--- src/components/projectList.js
+++ src/components/projectList.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types'
 import moment from 'moment';
 
 import SkillList from './skillList';
-import { FaDownload } from 'react-icons/fa';
 import '../styles/components/projectList.scss';
 
 
@@ -18,15 +17,18 @@ class ProjectList extends React.Component {
   }
 
   render = () => (
-    <div className={'work-list'}>
+    <div className={'project-list'}>
       <ul>
         {this.props.list.map(item => (
-            <li className='work-item'>
+            <li className='project-item'>
                 <div className={'work-image'} style={{backgroundImage: `url(${item.image})`}}></div>
                 <div className={'details'}>
                     <h3><a href={item.url}>{item.title}</a></h3>
                     <div className={'dates'}>
                         <span>{item.date} &mdash; { item.endDate ? moment(new Date(item.endDate)).format("MMM YYYY") : 'Present'}</span>
+                    </div>
+                    <div className={'annotation'}>
+                        <p>{item.annotation}</p>
                     </div>
                     <SkillList skills={item.skills}/>
                 </div>
